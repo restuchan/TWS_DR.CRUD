cb03d15be6325bfc354aa99f5f7651b9ddad1070
restuchan <restu.chan188@gmail.com>
2026-04-14 11:42:17 +0700

    menambahkan button load dan logic nya

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -49,12 +49,12 @@ namespace CRUDMahasiswaADO
         private void btnDelete_Click(object sender, EventArgs e)
         {
             try
-            {                
+            {
                 if (conn.State == System.Data.ConnectionState.Closed)
                 {
                     conn.Open();
                 }
-              
+
                 DialogResult resultConfirm = MessageBox.Show(
                     "Yakin ingin menghapus data?",
                     "Konfirmasi",
@@ -74,7 +74,7 @@ namespace CRUDMahasiswaADO
                     {
                         MessageBox.Show("Data berhasil dihapus!");
                         ClearForm();
-                        btnLoad.PerformClick();                       
+                        btnLoad.PerformClick();
                     }
                     else
                     {
@@ -87,6 +87,7 @@ namespace CRUDMahasiswaADO
                 MessageBox.Show("Terjadi kesalahan: " + ex.Message);
             }
         }
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -124,7 +125,7 @@ namespace CRUDMahasiswaADO
 
                 dataGridView1.Rows.Clear();
                 dataGridView1.Columns.Clear();
-                
+
                 dataGridView1.Columns.Add("NIM", "NIM");
                 dataGridView1.Columns.Add("Nama", "Nama");
                 dataGridView1.Columns.Add("JenisKelamin", "Jenis Kelamin");
@@ -132,21 +133,21 @@ namespace CRUDMahasiswaADO
                 dataGridView1.Columns.Add("Alamat", "Alamat");
                 dataGridView1.Columns.Add("KodeProdi", "Kode Prodi");
 
-                
+
                 string query = "SELECT * FROM Mahasiswa";
                 SqlCommand cmd = new SqlCommand(query, conn);
 
-                
+
                 SqlDataReader reader = cmd.ExecuteReader();
 
-                
+
                 while (reader.Read())
                 {
                     dataGridView1.Rows.Add(
                         reader["NIM"].ToString(),
                         reader["Nama"].ToString(),
                         reader["JenisKelamin"].ToString(),
-                       
+
 
                         Convert.ToDateTime(reader["TanggalLahir"]).ToShortDateString(),
                         reader["Alamat"].ToString(),
@@ -154,7 +155,7 @@ namespace CRUDMahasiswaADO
                     );
                 }
 
-              
+
                 reader.Close();
             }
             catch (Exception ex)
@@ -163,152 +164,6 @@ namespace CRUDMahasiswaADO
             }
         }
 
-        private void btnInsert_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                if (conn.State == System.Data.ConnectionState.Closed)
-                {
-                    conn.Open();
-                }
-
-                if (txtNIM.Text == "")
-                {
-                    MessageBox.Show("NIM harus diisi!");
-                    txtNIM.Focus();
-                    return;
-                }
-                if (txtNama.Text == "")
-                {
-                    MessageBox.Show("Nama harus diisi!");
-                    txtNama.Focus();
-                    return;
-                }
-                if (cmbJK.Text == "")
-                {
-                    MessageBox.Show("Jenis Kelamin harus dipilih!");
-                    cmbJK.Focus();
-                    return;
-                }
-                if (txtKodeProdi.Text == "")
-                {
-                    MessageBox.Show("Kode Prodi harus diisi!");
-                    txtKodeProdi.Focus();
-                    return;
-                }
-
-                string query = @"INSERT INTO Mahasiswa 
-                         (NIM, Nama, JenisKelamin, TanggalLahir, Alamat, KodeProdi, TanggalDaftar) 
-                         VALUES 
-                         (@NIM, @Nama, @JK, @TanggalLahir, @Alamat, @KodeProdi, @TanggalDaftar)";
-
-                SqlCommand cmd = new SqlCommand(query, conn);
-
-                cmd.Parameters.AddWithValue("@NIM", txtNIM.Text);
-                cmd.Parameters.AddWithValue("@Nama", txtNama.Text);
-                cmd.Parameters.AddWithValue("@JK", cmbJK.Text);
-                cmd.Parameters.AddWithValue("@TanggalLahir", dtpTanggalLahir.Value.Date);
-                cmd.Parameters.AddWithValue("@Alamat", txtAlamat.Text);
-                cmd.Parameters.AddWithValue("@KodeProdi", txtKodeProdi.Text);
-                cmd.Parameters.AddWithValue("@TanggalDaftar", DateTime.Now);
-
-                int result = cmd.ExecuteNonQuery();
-
-                if (result > 0)
-                {
-                    MessageBox.Show("Data berhasil diupdate!");
-                    ClearForm();
-                    btnLoad.PerformClick();
-                }
-                else
-                {
-                    MessageBox.Show("Data tidak ditemukan");
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Terjadi kesalahan: " + ex.Message);
-            }
-        }
-
-        private void btnUpdate_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                if (conn.State == System.Data.ConnectionState.Closed)
-                {
-                    conn.Open();
-                }
-
-                string query = @"UPDATE Mahasiswa
-                                SET Nama = @Nama, 
-                                    JenisKelamin = @JK, 
-                                    TanggalLahir = @TanggalLahir, 
-                                    Alamat = @Alamat, 
-                                    KodeProdi = @KodeProdi
-                                WHERE NIM = @NIM";
-
-                SqlCommand cmd = new SqlCommand(query, conn);
-
-                cmd.Parameters.AddWithValue("@NIM", txtNIM.Text);
-                cmd.Parameters.AddWithValue("@Nama", txtNama.Text);
-                cmd.Parameters.AddWithValue("@JK", cmbJK.Text);
-                cmd.Parameters.AddWithValue("@TanggalLahir", dtpTanggalLahir.Value.Date);
-                cmd.Parameters.AddWithValue("@Alamat", txtAlamat.Text);
-                cmd.Parameters.AddWithValue("@KodeProdi", txtKodeProdi.Text);
-
-                int result = cmd.ExecuteNonQuery();
-
-                if (result > 0)
-                {
-                    MessageBox.Show("Data berhasil diupdate!");
-                    ClearForm();
-                    btnUpdate.PerformClick();
-                }
-                else
-                {
-                    MessageBox.Show("Data tidak ditemukan!");
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Terjadi kesalahan: " + ex.Message);
-            }
-
-        }
-
-        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
-            if (e.RowIndex >= 0)
-            {
-                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
-
-                txtNIM.Text = row.Cells["NIM"].Value.ToString();
-                txtNama.Text = row.Cells["Nama"].Value.ToString();
-                cmbJK.Text = row.Cells["JenisKelamin"].Value.ToString();
-                dtpTanggalLahir.Value = Convert.ToDateTime(row.Cells["TanggalLahir"].Value);
-                txtAlamat.Text = row.Cells["Alamat"].Value.ToString();
-                txtKodeProdi.Text = row.Cells["KodeProdi"].Value.ToString();
-            }
-        }
-
-        private void ClearForm()
-        {
-            txtNIM.Clear(); 
-            txtNama.Clear();
-            cmbJK.SelectedIndex = -1;
-            txtAlamat.Clear();
-            txtKodeProdi.Clear();
-            dtpTanggalLahir.Value = DateTime.Now;
-            txtNIM.Focus();
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-    }
-        
- }
+}
 
 
